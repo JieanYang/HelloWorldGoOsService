a428001b7de3ce2e86e2685b59b78b1f4b5c254d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2023-06-20 10:50:24 +0200

    Add comments

--- src/ProcessCheck/process_check_linux.go
+++ src/ProcessCheck/process_check_linux.go
@@ -5,6 +5,8 @@ import "syscall"
 type ProcessCheckerLinux struct{}
 
 func (p ProcessCheckerLinux) ProcessExists(pid int) (bool, error) {
+	// source: https://docs.oracle.com/cd/E86824_01/html/E54765/kill-2.html
+	// The kill() function sends a signal to a process or a group of processes. The process or group of processes to which the signal is to be sent is specified by pid. The signal that is to be sent is specified by sig and is either one from the list given in signal (see signal.h(3HEAD)), or 0. If sig is 0 (the null signal), error checking is performed but no signal is actually sent. This can be used to check the validity of pid.
 	err := syscall.Kill(pid, 0)
 	if err == nil {
 		return true, nil
